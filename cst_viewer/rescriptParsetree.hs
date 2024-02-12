83fb954d37dc2715074c001048520ad09d339439
Soham kapileshwar <sohamkapileshwar2@gmail.com>
2024-02-12 16:20:18 +0530

    Rescript Parsetree types complete

--- cst_viewer/rescriptParsetree.hs
+++ cst_viewer/rescriptParsetree.hs
@@ -43,17 +43,181 @@ data Position = Position
     , posCnum :: Int
     }
 
-type Loc a = 
-    { txt :: a
-    , loc :: Location
-    }
-
 -- Longident Types
 
-data Longident = 
+data Longident =
     Lident String
   | Ldot Longident String
   | Lapply Longident Longident
+
+-- Module Types
+
+data ModuleBinding = ModuleBinding
+    { pmbName :: Loc String
+    , pmbExpr :: ModuleExpr
+    , pmbAttributes :: Attributes
+    , pmbLoc :: Location
+    }
+
+data ModuleExpr = ModuleExpr
+    { pmodDesc :: ModuleExprDesc
+    , pmodLoc :: Location
+    , pmodAttributes :: Attributes
+    }
+
+data ModuleExprDesc
+    = PmodIdent (Loc Longident)
+    | PmodStructure Structure
+    | PmodFunctor (Loc String) (Maybe ModuleType) ModuleExpr
+    | PmodApply ModuleExpr ModuleExpr
+    | PmodConstraint ModuleExpr ModuleType
+    | PmodUnpack Expression
+    | PmodExtension Extension
+
+data ModuleType = ModuleType
+    { pmtyDesc :: ModuleTypeDesc
+    , pmtyLoc :: Location
+    , pmtyAttributes :: Attributes
+    }
+
+data ModuleTypeDesc
+    = PmtyIdent (Loc Longident)
+    | PmtySignature Signature
+    | PmtyFunctor (Loc String) (Maybe ModuleType) ModuleType
+    | PmtyWith ModuleType [WithConstraint]
+    | PmtyTypeof ModuleExpr
+    | PmtyExtension Extension
+    | PmtyAlias (Loc Longident)
+
+data ModuleDeclaration = ModuleDeclaration
+    { pmdName :: Loc String
+    , pmdType :: ModuleType
+    , pmdAttributes :: Attributes
+    , pmdLoc :: Location
+    }
+
+data ModuleTypeDeclaration = ModuleTypeDeclaration
+    { pmtdName :: Loc String
+    , pmtdType :: Maybe ModuleType
+    , pmtdAttributes :: Attributes
+    , pmtdLoc :: Location
+    }
+
+-- Open Description
+
+data OpenDescription = OpenDescription
+    { popenLid :: Loc Longident
+    , popenOverride :: OverrideFlag
+    , popenLoc :: Location
+    , popenAttributes :: Attributes
+    }
+
+-- Include Description
+
+data IncludeDescription = IncludeDescription IncludeInfos
+
+data IncludeInfos a = IncludeInfos
+    { pinclMod :: a
+    , pinclLoc :: Location
+    , pinclAttributes :: Attributes
+    }
+
+data IncludeDeclaration = IncludeDeclaration (ModuleExpr (IncludeInfos ModuleExpr))
+
+-- With Constraint
+
+data WithConstraint
+    = PwithType (Loc Longident) TypeDeclaration
+    | PwithModule (Loc Longident) (Loc Longident)
+    | PwithTypeSubst (Loc Longident) TypeDeclaration
+    | PwithModSubst (Loc Longident) (Loc Longident)
+
+
+-- Class Types
+
+type ClassTypeDeclaration = ClassType ClassInfos
+
+data ClassInfos a = ClassInfos
+    { pciVirt :: VirtualFlag
+    , pciParams :: [(CoreType, Variance)]
+    , pciName :: Loc String
+    , pciExpr :: a
+    , pciLoc :: Location
+    , pciAttributes :: Attributes
+    }
+
+data ClassStructure = ClassStructure
+    { pcstrSelf :: Pattern
+    , pcstrFields :: [ClassField]
+    }
+
+data ClassField = ClassField
+    { pcfDesc :: ClassFieldDesc
+    , pcfLoc :: Location
+    , pcfAttributes :: Attributes
+    }
+
+data ClassFieldDesc
+    = PcfInherit ()
+    | PcfVal (Loc Label) MutableFlag ClassFieldKind
+    | PcfMethod (Loc Label) PrivateFlag ClassFieldKind
+    | PcfConstraint (CoreType, CoreType)
+    | PcfInitializer Expression
+    | PcfAttribute Attribute
+    | PcfExtension Extension
+
+data ClassFieldKind
+    = CfkVirtual CoreType
+    | CfkConcrete OverrideFlag Expression
+
+data ClassType = ClassType
+    { pctyDesc :: ClassTypeDesc
+    , pctyLoc :: Location
+    , pctyAttributes :: Attributes
+    }
+
+data ClassTypeDesc
+    = PctyConstr (Loc Longident) [CoreType]
+    | PctySignature ClassSignature
+    | PctyArrow ArgLabel CoreType ClassType
+    | PctyExtension Extension
+    | PctyOpen OverrideFlag (Loc Longident) ClassType
+
+data ClassSignature = ClassSignature
+    { pcsigSelf :: CoreType
+    , pcsigFields :: [ClassTypeField]
+    }
+
+data ClassTypeField = ClassTypeField
+    { pctfDesc :: ClassTypeFieldDesc
+    , pctfLoc :: Location
+    , pctfAttributes :: Attributes
+    }
+
+data ClassTypeFieldDesc
+    = PctfInherit ClassType
+    | PctfVal (Loc Label) MutableFlag VirtualFlag CoreType
+    | PctfMethod (Loc Label) PrivateFlag VirtualFlag CoreType
+    | PctfConstraint (CoreType, CoreType)
+    | PctfAttribute Attribute
+    | PctfExtension Extension
+
+data ClassExpr = ClassExpr
+    { pclDesc :: ClassExprDesc
+    , pclLoc :: Location
+    , pclAttributes :: Attributes
+    }
+
+data ClassExprDesc
+    = PclConstr (Loc Longident) [CoreType]
+    | PclStructure ClassStructure
+    | PclFun ArgLabel (Maybe Expression) Pattern ClassExpr
+    | PclApply ClassExpr [(ArgLabel, Expression)]
+    | PclLet RecFlag [ValueBinding] ClassExpr
+    | PclConstraint ClassExpr ClassType
+    | PclExtension Extension
+    | PclOpen OverrideFlag (Loc Longident) ClassExpr
+
 
 -- Expression Types
 
@@ -63,7 +227,7 @@ data Expression = Expression
     , pexpAttributes :: Attributes
     }
 
-data Expression
+data ExpressionDesc
   = PexpIdent (Loc Longident)
   | PexpConstant Constant
   | PexpLet RecFlag [ValueBinding] Expression
@@ -160,28 +324,15 @@ data CoreTypeDesc
   | PtypPackage PackageType
   | PtypExtension Extension
 
--- ArgLabel Types
+-- Package Type
 
-data ArgLabel
-  = Nolabel
-  | Labelled String
-  | Optional String
+type PackageType = (Loc Longident, [(Loc Longident, CoreType)])
 
 -- Object Field Types
 
 data ObjectField
   = Otag (Location Label) Attributes CoreType
   | Oinherit CoreType
-
--- Label
-
-type Label = String
-
--- Closed Flag Types
-
-data ClosedFlag
-  = Closed
-  | Open
 
 -- Loc Types
 
@@ -193,18 +344,6 @@ data RowField
   = Rtag (Loc Label) Attributes Bool [CoreType]
   | Rinherit CoreType
 
--- Constant Types
-
-data Constant = 
-    PconstInteger String (Maybe Char)
-  | PconstChar Int
-  | PconstString String (Maybe String)
-  | PconstFloat String (Maybe Char)
-
--- RecFlag Types
-
-data RecFlag = Nonrecursive | Recursive
-
 -- Value Binding Types
 
 data ValueBinding = ValueBinding
@@ -212,6 +351,14 @@ data ValueBinding = ValueBinding
     , pvbExpr :: Expression
     , pvbAttributes :: Attributes
     , pvbLoc :: Location
+    }
+
+-- Case Types
+
+data Case = Case
+    { pcLhs :: Pattern
+    , pcGuard :: Maybe Expression
+    , pcRhs :: Expression
     }
 
 -- Pattern Types
@@ -245,6 +392,33 @@ data PatternDesc =
 -- Extension Types
 
 type Extension = (Loc Longident) Payload
+
+data ExtensionConstructor = ExtensionConstructor
+  {
+    pextName :: Loc String
+  , pextKind :: ExtensionConstructorKind
+  , pextLoc :: Location
+  , pextAttributes :: Attributes
+  }
+
+data ExtensionConstructorKind =
+    PextDecl ConstructorArguments (Maybe CoreType)
+  | PextRebind (Loc Longident)
+
+
+-- Constructor Declaration and Arguments Types
+
+data ConstructorDeclaration = ConstructorDeclaration
+    { pcdName :: Loc String
+    , pcdArgs :: ConstructorArguments
+    , pcdRes :: Maybe CoreType
+    , pcdLoc :: Location
+    , pcdAttributes :: Attributes
+    }
+
+data ConstructorArguments
+    = PcstrTuple [CoreType]
+    | PcstrRecord [LabelDeclaration]
 
 -- Value Description Types
 
@@ -311,16 +485,23 @@ data LabelDeclaration = LabelDeclaration
     , pldAttributes :: Attributes
     }
 
--- Mutable Flag Types
+-- AST TYPES --
 
-data MutableFlag = Immutable | Mutable
+-- Constant Types
 
--- Variance Types
+data Constant = 
+    PconstInteger String (Maybe Char)
+  | PconstChar Int
+  | PconstString String (Maybe String)
+  | PconstFloat String (Maybe Char)
 
-data Variance
-  = Covariant
-  | Contravariant
-  | Invariant
+-- RecFlag Types
+
+data RecFlag = Nonrecursive | Recursive
+
+-- Direction Flag Types
+
+data DirectionFlag = Upto | Downto
 
 -- Private Flag Types
 
@@ -328,10 +509,45 @@ data PrivateFlag
   = Private
   | Public
 
+-- Mutable Flag Types
 
+data MutableFlag = Immutable | Mutable
 
+-- Virtual Flag Types
 
+data VirtualFlag = Virtual | Concrete
 
+-- Override Flag Types
 
+data OverrideFlag = Override | Fresh
 
+-- Closed Flag Types
 
+data ClosedFlag
+  = Closed
+  | Open
+
+-- Label
+
+type Label = String
+
+-- ArgLabel Types
+
+data ArgLabel
+  = Nolabel
+  | Labelled String
+  | Optional String
+
+-- Loc Type
+
+type Loc a = 
+    { txt :: a
+    , loc :: Location
+    }
+
+-- Variance Types
+
+data Variance
+  = Covariant
+  | Contravariant
+  | Invariant
