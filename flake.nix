b4a9212a972b69229952952fdf6b65d6cb3db7f0
Soham kapileshwar <sohamkapileshwar2@gmail.com>
2024-02-23 14:09:21 +0530

    Updated Flakes to support default systems

--- flake.nix
+++ flake.nix
@@ -3,40 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-      shell = pkgs.mkShell {
-        buildInputs = with pkgs; [
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        shell = pkgs.mkShell {
+          buildInputs = with pkgs; [
 
-          git
-          coreutils
-          curl
+            git
+            coreutils
+            curl
 
-          # Haskell packages
-          haskell.compiler.ghc98
-          cabal-install
+            # Haskell packages
+            haskell.compiler.ghc98
+            cabal-install
 
-          # OCaml packages
-          ocaml
-          dune_3
-          opam
-        ];
+            # OCaml packages
+            ocaml
+            dune_3
+            opam
+          ];
 
-        # Shell hook to print message when entering the environment
-        shellHook = ''
-          PS1='\[\033[0;32m\]$(pwd)\[\033[0m\] (Nix Shell) \n\[\033[0;32m\]\$\[\033[0m\] '
-          export TERM="xterm-256color"
-        '';
-      };
-    in
-    {
-      devShell = shell;
-      # packages.${system}.default = self.devShell;
-      # devShells.aarch64-darwin.default = self.devShell;
-      devShells.x86_64-darwin.default = self.devShell;
-    };
+          # Shell hook to print message when entering the environment
+          shellHook = ''
+            PS1='\[\033[0;32m\]$(pwd)\[\033[0m\] (Nix Shell) \n\[\033[0;32m\]\$\[\033[0m\] '
+            export TERM="xterm-256color"
+          '';
+        };
+      in
+        {
+          devShells.default = shell;
+        }
+    );
 }
