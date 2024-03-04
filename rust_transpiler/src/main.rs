8def9dd572e0454c606cb19ae37689f17a19c7cb
Soham kapileshwar <sohamkapileshwar2@gmail.com>
2024-03-04 14:30:08 +0530

    Testing

--- rust_transpiler/src/main.rs
+++ rust_transpiler/src/main.rs
@@ -1,4 +1,20 @@
-fn main() {
-    println!("Hello, worl !");
+mod purescript_types;
 
+use purescript_types::purescript;
+
+fn main() {
+    println!("Hello, world!");
+    let mut s= String::from("Hello");
+
+    s.push_str(", Again");
+    test(&mut s);
+    println!("{s}");
+
+    println!("Testing Purescript Types module");
+    purescript();
+}
+
+fn test(some_string: &mut String) {
+    some_string.push_str(" World");
+    println!("{some_string}");
 }
---
+++ rust_transpiler/src/purescript_types.rs
@@ -0,0 +1,3 @@
+pub fn purescript() {
+    println!("Testing Purescript Types module from purescript_types.rs")
+}
