c9fe6b560d14ab90b87fe072ef7208b95188f57f
ashu414 <ashutosh.singh1@juspay.in>
2024-03-04 13:50:07 +0530

    added cargo watch in rust

--- rust_transpiler/src/main.rs
+++ rust_transpiler/src/main.rs
@@ -1,16 +1,4 @@
 fn main() {
-    println!("Hello, world!");
-    let mut s= String::from("Hello");
-    let y = s;
+    println!("Hello, worl !");
 
-    s.push_str(", Again");
-    let bytes = s.as_bytes()
-    println!("{bytes}");
-    test(&mut s);
-    println!("{s}");
-}
-
-fn test(some_string: &mut String) {
-    some_string.push_str(" World");
-    println!("{some_string}");
 }
